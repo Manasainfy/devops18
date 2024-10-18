93dcd7b5356c63b5ea504c8b32764ebe86b89d29
Manasa <manasabingumalla59@gmail.com>
2024-10-18 16:07:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,6 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    security_groups = [aws_security_group.web_server.id]
     availability_zones    = ["us-east-1a", "us-east-1b"] 
     launch_template {
         id      = aws_launch_template.web_server_as.id
